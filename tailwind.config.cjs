0a11db53e6ca38cdff1bde8b3b93f93763cb56ff
J.B.C <juanbicortelezzi@gmail.com>
2022-09-19 01:01:33 -0300

    colorscheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,19 +7,17 @@ module.exports = {
   daisyui: {
     logs: false,
     themes: [
-      "night",
-      "light",
       {
-        mytheme: {
-          primary: "#3ABFF8",
-          secondary: "#828DF8",
-          accent: "#F471B5",
-          neutral: "#1D283A",
-          "base-100": "#0F1729",
-          info: "#0CA6E9",
-          success: "#2BD4BD",
-          warning: "#F4C152",
-          error: "#FB6F84",
+        light: {
+          primary: "#050066",
+          secondary: "#238e3f",
+          accent: "#FFB09F",
+          neutral: "#ffffff",
+          "base-100": "#f4f5fa",
+          info: "#28AFB0",
+          success: "#21A179",
+          warning: "#FF8811",
+          error: "#d44853",
         },
       },
     ],
